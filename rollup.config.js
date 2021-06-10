aa9b011e3ad245cdf7ad9358452e1ea56fd80347
beginor <beginor@qq.com>
2021-06-10 15:39:12 +0800

    add packages.

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import { terser } from 'rollup-plugin-terser';
+import { ngcPlugin } from 'rollup-plugin-ngc';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -20,7 +21,8 @@ export default {
   treeshake: production,
   external: [],
   plugins: [
-    typescript({ tsconfig: 'tsconfig.json', sourceMap: !production }),
+    // typescript({ tsconfig: 'tsconfig.json', sourceMap: !production }),
+    ngcPlugin({ rootDir: './src' }),
     css({ output: 'main.css' }),
     alias({}),
     nodeResolve({ mainFields: ['module', 'main'] }),
@@ -28,7 +30,18 @@ export default {
       include: []
     }),
     production && terser({
-      format: { comments: false }
+      format: {
+        ascii_only: true,
+        comments: false,
+        webkit: true
+      },
+      compress: {
+        pure_getters: true,
+        passes: 3,
+        global_defs: {
+          ngDevMode: false
+        }
+      }
     })
   ],
   preserveEntrySignatures: false
