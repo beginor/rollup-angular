73310eae1ac9fae82be042fb4e0e944f483d7e33
beginor <beginor@qq.com>
2021-07-01 16:59:50 +0800

    remove failed ngc plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,18 @@
 import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+// import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import { terser } from 'rollup-plugin-terser';
-// import { ngcPlugin } from 'rollup-plugin-ngc';
-import { ngc } from './rollup/plugin-ngc/index';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
-const production = false;// !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH;
 
 /** @type { import('rollup').RollupOptions } */
 export default [
   {
-    input: ['./projects/web/src/main.ts'],
+    input: ['./dist/out-tsc/web/main.js'],
     output: {
       dir: 'dist',
       chunkFileNames: production ? "chunks/[name]-[hash].js" : "chunks/[name].js",
@@ -35,7 +33,6 @@ export default [
     ],
     plugins: [
       // typescript({ tsconfig: 'tsconfig.json', sourceMap: !production }),
-      ngc({ project: 'projects/web/tsconfig.app.json'}),
       css({ output: 'main.css' }),
       alias({}),
       nodeResolve({ mainFields: ['module', 'main'] }),
